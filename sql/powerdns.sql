d0fe9e7593a5b82cfe03bf8dc968f46921c26e75
Alexey Gordeyev <aleksej@gordejev.lv>
2016-04-17 13:02:47 +0300

    add installation description

--- sql/powerdns.sql
+++ sql/powerdns.sql
@@ -1,32 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 4.4.15.5
--- http://www.phpmyadmin.net
---
--- Хост: localhost
--- Время создания: Апр 17 2016 г., 12:26
--- Версия сервера: 5.5.47-MariaDB
--- Версия PHP: 5.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- База данных: `powerdns`
---
 CREATE DATABASE IF NOT EXISTS `powerdns` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `powerdns`;
 
 -- --------------------------------------------------------
-
---
--- Структура таблицы `comments`
---
 
 DROP TABLE IF EXISTS `comments`;
 CREATE TABLE IF NOT EXISTS `comments` (
@@ -41,10 +25,6 @@ CREATE TABLE IF NOT EXISTS `comments` (
 
 -- --------------------------------------------------------
 
---
--- Структура таблицы `cryptokeys`
---
-
 DROP TABLE IF EXISTS `cryptokeys`;
 CREATE TABLE IF NOT EXISTS `cryptokeys` (
   `id` int(11) NOT NULL,
@@ -56,10 +36,6 @@ CREATE TABLE IF NOT EXISTS `cryptokeys` (
 
 -- --------------------------------------------------------
 
---
--- Структура таблицы `domainmetadata`
---
-
 DROP TABLE IF EXISTS `domainmetadata`;
 CREATE TABLE IF NOT EXISTS `domainmetadata` (
   `id` int(11) NOT NULL,
@@ -69,10 +45,6 @@ CREATE TABLE IF NOT EXISTS `domainmetadata` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
-
---
--- Структура таблицы `domains`
---
 
 DROP TABLE IF EXISTS `domains`;
 CREATE TABLE IF NOT EXISTS `domains` (
@@ -86,10 +58,6 @@ CREATE TABLE IF NOT EXISTS `domains` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
-
---
--- Структура таблицы `records`
---
 
 DROP TABLE IF EXISTS `records`;
 CREATE TABLE IF NOT EXISTS `records` (
@@ -108,10 +76,6 @@ CREATE TABLE IF NOT EXISTS `records` (
 
 -- --------------------------------------------------------
 
---
--- Структура таблицы `supermasters`
---
-
 DROP TABLE IF EXISTS `supermasters`;
 CREATE TABLE IF NOT EXISTS `supermasters` (
   `ip` varchar(64) NOT NULL,
@@ -121,10 +85,6 @@ CREATE TABLE IF NOT EXISTS `supermasters` (
 
 -- --------------------------------------------------------
 
---
--- Структура таблицы `tsigkeys`
---
-
 DROP TABLE IF EXISTS `tsigkeys`;
 CREATE TABLE IF NOT EXISTS `tsigkeys` (
   `id` int(11) NOT NULL,
@@ -133,94 +93,65 @@ CREATE TABLE IF NOT EXISTS `tsigkeys` (
   `secret` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Индексы сохранённых таблиц
---
 
---
--- Индексы таблицы `comments`
---
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`id`),
   ADD KEY `comments_domain_id_idx` (`domain_id`),
   ADD KEY `comments_name_type_idx` (`name`,`type`),
   ADD KEY `comments_order_idx` (`domain_id`,`modified_at`);
 
---
--- Индексы таблицы `cryptokeys`
---
+
 ALTER TABLE `cryptokeys`
   ADD PRIMARY KEY (`id`),
   ADD KEY `domainidindex` (`domain_id`);
 
---
--- Индексы таблицы `domainmetadata`
---
+
 ALTER TABLE `domainmetadata`
   ADD PRIMARY KEY (`id`),
   ADD KEY `domainmetadata_idx` (`domain_id`,`kind`);
 
---
--- Индексы таблицы `domains`
---
+
 ALTER TABLE `domains`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name_index` (`name`);
 
---
--- Индексы таблицы `records`
---
+
 ALTER TABLE `records`
   ADD PRIMARY KEY (`id`),
   ADD KEY `nametype_index` (`name`,`type`),
   ADD KEY `domain_id` (`domain_id`),
   ADD KEY `recordorder` (`domain_id`,`ordername`);
 
---
--- Индексы таблицы `supermasters`
---
+
 ALTER TABLE `supermasters`
   ADD PRIMARY KEY (`ip`,`nameserver`);
 
---
--- Индексы таблицы `tsigkeys`
---
+
 ALTER TABLE `tsigkeys`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `namealgoindex` (`name`,`algorithm`);
 
---
--- AUTO_INCREMENT для сохранённых таблиц
---
 
---
--- AUTO_INCREMENT для таблицы `comments`
---
 ALTER TABLE `comments`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `cryptokeys`
---
+
+
 ALTER TABLE `cryptokeys`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `domainmetadata`
---
+
+
 ALTER TABLE `domainmetadata`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `domains`
---
+
+
 ALTER TABLE `domains`
   MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `records`
---
+
+
 ALTER TABLE `records`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `tsigkeys`
---
+
+
 ALTER TABLE `tsigkeys`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
